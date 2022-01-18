181be7cffb23fafd09a6a325666f02b7b925b63a
PPB InfoSec Engineering <63779195+fopinappb@users.noreply.github.com>
2022-01-18 13:18:48 +0000

    random token template missing update

--- notifications/static/django/forms/widgets/randomgeneratetext.js
+++ notifications/static/django/forms/widgets/randomgeneratetext.js
@@ -1,9 +1,11 @@
-django.jQuery(function() {
+// django jquery.init might have been loaded already or not...
+(typeof django.jQuery !== "undefined" ? django.jQuery : $)(function() {
     django.jQuery('#id_external_token_generate').click(function() {
-    var token = '';
-    while (token.length < 40) {
-        token += Math.random().toString(36).substring(2);
-    }
-    django.jQuery('#id_external_token').val(token.substring(0, 40));
+        var token = '';
+        while (token.length < 40) {
+            token += Math.random().toString(36).substring(2);
+        }
+        django.jQuery('#id_external_token').val(token.substring(0, 40));
+        return false;
     });
 });
