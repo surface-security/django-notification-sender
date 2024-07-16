39e58ad0af08550c9beb710272108c87b4b9c22e
Duarte Duarte <duarte.duarte@blip.pt>
2024-07-16 11:51:25 +0100

    0.0.6 -> 0.1.0

--- notifications/__init__.py
+++ notifications/__init__.py
@@ -1,10 +1,10 @@
-__version__ = '0.0.6'
+__version__ = "0.1.0"
 
 # set default_app_config when using django earlier than 3.2
 try:
     import django
 
     if django.VERSION < (3, 2):
-        default_app_config = 'notifications.apps.NotificationsConfig'
+        default_app_config = "notifications.apps.NotificationsConfig"
 except ImportError:
     pass
