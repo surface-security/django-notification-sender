7f3e1b3e73492438ccb965abc650625ee5478f0f
Duarte Duarte <duarte.duarte@blip.pt>
2024-07-16 13:28:00 +0100

    Drop support for Django 3.2
    https://www.djangoproject.com/download/#supported-versions

--- notifications/__init__.py
+++ notifications/__init__.py
@@ -1,10 +1,1 @@
 __version__ = "0.1.0"
-
-# set default_app_config when using django earlier than 3.2
-try:
-    import django
-
-    if django.VERSION < (3, 2):
-        default_app_config = "notifications.apps.NotificationsConfig"
-except ImportError:
-    pass
